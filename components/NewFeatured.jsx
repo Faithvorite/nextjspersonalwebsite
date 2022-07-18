ad67a2940a6442ac5bd83700be6df962cefcced3
Faithvorite <57932015+Faithvorite@users.noreply.github.com>
2022-07-18 17:23:59 -0500

    d

--- components/NewFeatured.jsx
+++ components/NewFeatured.jsx
@@ -4,45 +4,40 @@ import Image from "next/image";
 import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faQuoteLeft, faQuoteRight, faQuoteRightAlt } from '@fortawesome/free-solid-svg-icons';
-import horsetail from "/img/Art/horsetailfalls2.JPG"
-import mistyroad from "/img/Art/mistyroadframed.JPG"
-import witness from "/img/Art/unnamed.JPG"
-import tote from "/img/Art/hannahtote.JPG"
-import lavendar from "/img/Art/francelavendarfields.JPG"
-import transparency from "/img/Art/transparency.JPG"
+
 const NewFeatured = () => {
     const [index,setIndex]= useState(0);
     const images = [
         {
-        img:{horsetail},
+        img:"/img/Art/horsetailfalls2.JPG",
         desc: '30" x 40" oil on canvas',
-        review: " Excellent! This painting has brought life into an empty room. So very happy with the result ",
+        review: "Excellent! This painting has brought life into an empty room. So very happy with the result",
         author: "-Minh Hong"
         },
         {
-        img: {mistyroad},
+        img: "/img/Art/mistyroadframed.JPG",
         desc: "Oil painting",
-        review:' Outstanding! ',
+        review:' Outstanding!',
         author: "-Minh Hong"
         },
         {
-        img: {witness},
+        img: "/img/Art/unnamed.JPG",
         desc: "Oil painting",
-        review: " Top notch ",
+        review: "Top notch",
         author: "-Minh Hong"
         },
         {
-        img: {tote},
+        img:"/img/Art/hannahtote.JPG",
         desc: "Oil painting",
-        review: " Incredible ",
+        review: "Incredible",
         author: "-Minh Hong"
         },
-        {img:{ transparency} ,
-        review: " Incredible ",
+        {img: "/public/img/Art/transparency.JPG",
+        review: "Incredible",
         author: "-Minh Hong"
         },
         {
-        img: {lavendar},
+        img: "/public/img/Art/francelavendarfields.JPG",
         review: "Incredible",
         author: "-Minh Hong"
         },
@@ -65,7 +60,7 @@ const NewFeatured = () => {
           {images.map((images, i) => (
             <div className={styles.featuredContainer} key={i}>
               <div className={styles.imgContainer} key={i}>
-              <Image href="" src={images.img} key={i} alt="" layout="fill" objectFit="cover"/>
+              <Image src={images.img} key={i} alt="" layout="fill" objectFit="cover"/>
               </div>
               <div className={styles.textContainer}>
                 <div className={styles.desc}>{images.desc}</div>
