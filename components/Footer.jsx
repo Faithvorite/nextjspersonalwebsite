1449df473d69fa9ba0ef73f3d9a0c5d4c5f5cbca
Faithvorite <57932015+Faithvorite@users.noreply.github.com>
2022-07-21 16:43:04 -0500

    email

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from "../styles/Footer.module.css";
 import Image from "next/image";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAddressBook, faBookmark, faCalendarCheck, faComment, faContactBook } from '@fortawesome/free-regular-svg-icons';
+import emailjs from '@emailjs/browser'
 
 const Footer = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm(
+      'service_9b951xl', 
+      'template_v8qrumr', 
+      form.current, 
+      'dVv23QUTshCDR1zwp')
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again')
+        }
+      )
+  }
+
   return (
     <div className={styles.container}>
          <div  className={styles.newsletterwrapper}>
             <div className={styles.title}>
                 GET IN TOUCH
             </div>
+            <form ref={form} onSubmit={sendEmail}>
                 <input type="email" className ={styles.formcontrol} placeholder="youremail@email.com"/>
-                <button type="submit" className={styles.btn}>Subscribe</button>
+                <button value="Send" type="submit" className={styles.btn}>Subscribe</button>
+            </form>
             
         </div>
         <div className={styles.contactme}>
@@ -26,18 +50,7 @@ const Footer = () => {
            <FontAwesomeIcon className={styles.icon} icon={faComment} />
                   </div>
                 </div>
-      {/* <div className={styles.seeMore}>
-                 <div className={styles.aboutme}>
-                    <div className={styles.imgcontainer}>
-                    <Image className={styles.img} src="/img/Art/me.JPG" alt="" layout="fill" />
-                    </div>
-                    <div className={styles.describeme}>
-                    Hello, my name is Faith Nguyen. I am a self taught artist and creative since birth. 
-                    </div>
-                </div>
-                
-      </div>
-    */}
+
   
 
     </div>
