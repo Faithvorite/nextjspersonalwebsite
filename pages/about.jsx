45d45402ce666dd5cde874fdb8fc8d14bfb6b9b2
Faithvorite <57932015+Faithvorite@users.noreply.github.com>
2022-07-23 15:58:16 -0500

    abt

--- pages/about.jsx
+++ pages/about.jsx
@@ -19,6 +19,15 @@ const about = () => {
     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem adipisci enim nostrum eius corrupti, alias iste, quod, consequatur itaque obcaecati deleniti error ducimus! Quasi explicabo amet possimus officiis est accusamus!
     
     </div>
+    <div className="aboutheader">Career Endeavors</div>
+          <p>
+     I graduated Cum Laude from UT Austin in 2019, with a B.S. in English and minor in Creative Writing. While English and art have long been my outlets for expression, in 2021 I began studying web development as well. Despite changes in my career path, my goal in life has remained the same - to better my ability to create and communicate.
+          </p>
+          <hr></hr>
+          <div className="aboutheader">Personal Pursuits</div>
+          <p>
+            I am a writer, artist, and programmer. 
+          </p>
     <div className="doodlebox">
 
     <Image placeholder="blur" className={styles.doodle} src={doodle} alt="" width="500px" height="500px" />
